4dc8cf9050ad146c78e1aa3e23105bdcaa0e3766
Justin Barrick <justin.m.barrick@gmail.com>
2018-09-30 21:25:15 +0000

    Do not set useragent on spotify or youtube, fallback to html title.

--- src/github.com/matrix-org/go-neb/services/music/music.go
+++ src/github.com/matrix-org/go-neb/services/music/music.go
@@ -4,12 +4,18 @@ package echo
 import (
 	"mvdan.cc/xurls"
 
+	"bytes"
 	"errors"
-	"sync"
+	"io"
+	"io/ioutil"
 	"fmt"
 	"net/url"
 	"net/http"
+	"strings"
+	"sync"
 	"time"
+
+	"golang.org/x/net/html"
 
 	cache "github.com/patrickmn/go-cache"
 	"github.com/Sirupsen/logrus"
@@ -28,7 +34,7 @@ type MetaData struct {
 	Title       string  `meta:"og:title"`
 	Description string  `meta:"og:description,description"`
 	Type        string  `meta:"og:type"`
-	URL         url.URL `meta:"og:url"`
+	URL         string  `meta:"og:url"`
 	Image       url.URL `meta:"og:image"`
 	VideoWidth  int64   `meta:"og:video:width"`
 	VideoHeight int64   `meta:"og:video:height"`
@@ -38,6 +44,55 @@ type MetaData struct {
 // Service represents the Echo service. It has no Config fields.
 type Service struct {
 	types.DefaultService
+}
+
+// https://siongui.github.io/2016/05/10/go-get-html-title-via-net-html/
+func isTitleElement(n *html.Node) bool {
+	return n.Type == html.ElementNode && n.Data == "title"
+}
+
+func traverse(n *html.Node) (string, bool) {
+	if isTitleElement(n) {
+		return n.FirstChild.Data, true
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		result, ok := traverse(c)
+		if ok {
+			return result, ok
+		}
+	}
+
+	return "", false
+}
+
+func getHtmlTitle(r io.Reader) (string, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return "", err
+	}
+
+	title, ok := traverse(doc)
+	if ok != true {
+		return "", errors.New("Could not parse title.")
+	}
+
+	return title, nil
+}
+
+func setUseragent(parsedUrl *url.URL, req *http.Request) {
+	defaultUseragent := []string{
+		"spotify.com", "youtube.com", "youtu.be",
+	}
+
+	for _, hostname := range defaultUseragent {
+		if hostname == parsedUrl.Host || strings.HasSuffix(parsedUrl.Host, "." + hostname) {
+			return
+		}
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36")
+	return
 }
 
 func fetchURL(client *gomatrix.Client, lock *sync.Mutex, urlCache *cache.Cache, fetchUrl string) (*MetaData, error) {
@@ -53,12 +108,17 @@ func fetchURL(client *gomatrix.Client, lock *sync.Mutex, urlCache *cache.Cache, 
 
 	httpClient := &http.Client{}
 
+	parsedUrl, err := url.Parse(fetchUrl)
+	if err != nil {
+		return nil, err
+	}
+
 	req, err := http.NewRequest("GET", fetchUrl, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0")
+	setUseragent(parsedUrl, req)
 
 	res, err := httpClient.Do(req)
 	if err != nil {
@@ -69,7 +129,12 @@ func fetchURL(client *gomatrix.Client, lock *sync.Mutex, urlCache *cache.Cache, 
 		return nil, errors.New(fmt.Sprintf("Status code %d is not 200", res.StatusCode))
 	}
 
-	err = m.Metabolize(res.Body, metadata)
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	err = m.Metabolize(bytes.NewBuffer(body), metadata)
 	if err != nil {
 		return nil, err
 	}
@@ -77,9 +142,20 @@ func fetchURL(client *gomatrix.Client, lock *sync.Mutex, urlCache *cache.Cache, 
 	if metadata.Image != (url.URL{}) {
 		resUpload, err := client.UploadLink(metadata.Image.String())
 		if err != nil {
-			return metadata, err
+			return nil, err
 		}
 		metadata.UploadedUrl = resUpload.ContentURI
+	}
+
+	if metadata.Title == "" {
+		metadata.Title, err = getHtmlTitle(bytes.NewBuffer(body))
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if metadata.URL == "" {
+		metadata.URL = fetchUrl
 	}
 
 	urlCache.Set(fetchUrl, metadata, cache.DefaultExpiration)
@@ -128,14 +204,14 @@ func (s *Service) Expansions(cli *gomatrix.Client) []types.Expansion {
 					)
 				}
 
-				if metadata.Description == "" {
-					logrus.Warning("No description!")
+				if metadata.Title == "" {
+					logrus.Warning("No description:", metadata)
 					return nil
 				}
 
 				return gomatrix.GetHTMLMessage(
 					"m.notice",
-					fmt.Sprintf("<a href=\"%s\">%s</a><br>%s", metadata.URL.String(), metadata.Title, metadata.Description),
+					fmt.Sprintf("<a href=\"%s\">%s</a><br>%s", metadata.URL, metadata.Title, metadata.Description),
 				)
 			},
 		},
